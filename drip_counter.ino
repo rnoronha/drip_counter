f763b811c2fc299fabce4d0413dc011c96329575
Rodrigo Noronha <rodrigo.w.noronha@gmail.com>
2015-11-26 21:32:34 -0600

    Added RTC module, prints it out now.

--- drip_counter.ino
+++ drip_counter.ino
@@ -1,12 +1,23 @@
 #include <JeeLib.h>
+#include <Wire.h>
+#include "RTClib.h"
 
 const int knockSensor = 2; 
 
 ISR(WDT_vect) { Sleepy::watchdogEvent(); } // Setup for low power waiting, though I don't actually do any
 
+RTC_DS1307 RTC;
+
 void setup() {
   pinMode(2, INPUT); // declare the ledPin as as OUTPUT
   Serial.begin(9600);       // use the serial port
+  Wire.begin();
+  RTC.begin();
+
+  if (!RTC.isrunning()) {
+    Serial.println("No RTC soz, might mean it's not running so gonna adjust it");
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
 }
 
 void loop() {
@@ -15,9 +26,14 @@ void loop() {
   delay(100);
   
   attachInterrupt(digitalPinToInterrupt(knockSensor), piezoEvent, RISING);
+  
   Sleepy::powerDown();
   
   Serial.println("woke up!");
+  
+  DateTime now = RTC.now();
+  Serial.println(now.unixtime());
+  delay(100);
 }
 
 void piezoEvent() {
--- readme.txt
+++ readme.txt
@@ -1,5 +1,4 @@
 Will at some point be code for a piezoelectric drip (or other knocking event) counter. 
 
 Requires the JeeLabs Sleepy library: https://github.com/jcw/jeelib
-
-
+And now requires an RTC and the associated RTC library; I'm using the Adafruit fork of JeeLab's RTC: https://github.com/adafruit/RTClib
