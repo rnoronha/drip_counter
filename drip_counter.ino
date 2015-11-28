82fed1ee13e9fd1b38d1158fb5bcb8899b42f03d
Rodrigo Noronha <rodrigo.w.noronha@gmail.com>
2015-11-28 10:56:59 -0600

    Playing around with the EEPROM

--- drip_counter.ino
+++ drip_counter.ino
@@ -1,12 +1,15 @@
 #include <JeeLib.h>
 #include <Wire.h>
-#include "RTClib.h"
+#include <EEPROM.h>
+#include <RTClib.h>
 
 const int knockSensor = 2; 
 
 ISR(WDT_vect) { Sleepy::watchdogEvent(); } // Setup for low power waiting, though I don't actually do any
 
 RTC_DS1307 RTC;
+
+int eeprom_addr = 0;
 
 void setup() {
   pinMode(2, INPUT); // declare the ledPin as as OUTPUT
@@ -20,6 +23,8 @@ void setup() {
   }
 }
 
+
+
 void loop() {
   Serial.println("I'm gonna sleep");
   //Have to delay before going to sleep, otherwise it cuts off the serial printing
@@ -30,10 +35,25 @@ void loop() {
   Sleepy::powerDown();
   
   Serial.println("woke up!");
-  
+  unsigned long start = micros();
   DateTime now = RTC.now();
+  unsigned long finish = micros();
   Serial.println(now.unixtime());
-  delay(100);
+  Serial.println(finish - start);
+  Serial.println(eeprom_addr);
+  EEPROM.write(eeprom_addr, now.unixtime());
+  eeprom_addr++;
+  
+  if(eeprom_addr % 4 == 0) {
+    Serial.println("eeprom dump: ");
+    for(int i = eeprom_addr - 4; i < eeprom_addr; i++) {
+      byte value = EEPROM.read(i);
+      Serial.print(i);
+      Serial.print("\t");
+      Serial.print(value, DEC);
+      Serial.println();
+    }
+  }
 }
 
 void piezoEvent() {
